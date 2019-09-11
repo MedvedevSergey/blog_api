4f053379b37f966fcee7aa3b3a2a0a4b692878d2
Sergey Medvedev <Sergey.Medvedev@cadfem-cis.ru>
2019-09-11 12:17:03 +0300

    Added filter_horizontal, list_display_links to  ArticleAdmin

--- article/admin.py
+++ article/admin.py
@@ -4,5 +4,7 @@ from .models import Article
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['title', 'created_at', 'updated_at']
+    list_display = ['id', 'title', 'created_at', 'updated_at']
+    list_display_links = ['id', 'title']
+    filter_horizontal = ['tags']
 
