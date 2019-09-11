20307e37fe4f4d308d6036e263919023e6735b62
Sergey Medvedev <Sergey.Medvedev@cadfem-cis.ru>
2019-09-11 14:32:18 +0300

    Added TagSerializer

--- article/serializers.py
+++ article/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework.serializers import ModelSerializer
-from article.models import Article
+from .models import Article, Tag
+
+
+class TagSerializer(ModelSerializer):
+
+    class Meta:
+        model = Tag
+        fields = '__all__'
 
 
 class ArticleSerializer(ModelSerializer):
+    tags = TagSerializer(many=True)
 
     class Meta:
         model = Article
