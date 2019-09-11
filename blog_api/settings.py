69db604f87298ebd9b0b178e333b06be9b0749ee
Sergey Medvedev <Sergey.Medvedev@cadfem-cis.ru>
2019-09-11 12:39:29 +0300

    Added new app 'Comment'

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'article',
     'account',
+    'comment',
 ]
 
 MIDDLEWARE = [
@@ -113,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
---
+++ comment/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey('auth.User', blank=True,
+                               on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    article = models.ForeignKey('article.Article', on_delete=models.CASCADE,
+                                related_name="comments")
+    parent = models.ForeignKey('self', blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.article.title
+
